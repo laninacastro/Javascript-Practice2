93deb7ccce062d0bc253727b80a0ec13eabea865
Belinda <90870640+laninacastro@users.noreply.github.com>
2022-02-28 22:03:27 -0500

    daily challenge

---
+++ practice day 1/script.js
@@ -0,0 +1,25 @@
+// display an alert dialog with the content: "Warning this message will self destruct in "
+
+alert("Warning this message will self destruct in")
+
+// 2. Display a "3..2..1.." countdown using 3 alert dialog boxes
+alert("3..");
+
+alert("2..");
+
+alert("1..");
+
+//3. This statement selects the <p> element and replaces it with "BOOM!"
+document.querySelector("p").textContent = "BOOM! THAT WAS EASY!";
+
+
+
+//4. lOG "mESSAGE DESTROYED TO THE CONSOLE"
+console.log("Message Destroyed!!")
+
+
+
+alert("Hello, world!");
+console.log("Hello from the console.");
+alert("Thanks for visiting my website");
+document.write("<h1>Welcome to my website</h1>")
--- script.js
+++
@@ -1,4 +0,0 @@
-alert("Hello, world!");
-console.log("Hello from the console.");
-alert("Thanks for visiting my website");
-document.write("<h1>Welcome to my website</h1>")
